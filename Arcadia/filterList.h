eb01f81eefb1fc8fb9f18be063322b6d72a33c16
Ashley <prosthetichead@gmail.com>
2014-04-03 23:11:36 +1030

    started filterList header

--- Arcadia/filterList.h
+++ Arcadia/filterList.h
@@ -7,9 +7,12 @@ class filterList
 private:
 	sf::RectangleShape rectangle;
 	int selectedItemNum;
+	std::vector<dbHandle::filterListItem> listOfItems;
 	
 public:
 	filterList(void);
 	~filterList(void);
+	void filterList::init(dbHandle& db_obj, float posX, float posY, int width, float height);
+	void filterList::addNewFilterItem(dbHandle::filterListItem);
 };
 
